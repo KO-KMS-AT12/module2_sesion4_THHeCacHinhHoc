0868b3ca9c6b88a5c0b2ee3cabf39782ecee49df
ko <vtg0502@gmail.com>
2019-05-03 16:25:48 +0700

    thuc hanh xay dung lop he cac hinh hoc

---
+++ src/main/java/HinhHoc/Circle.java
@@ -0,0 +1,38 @@
+package HinhHoc;
+
+public class Circle extends Shape {
+    private double radius = 1.0;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.pow(this.radius, 2) * Math.PI;
+    }
+
+    public double getPerimeter() {
+        return this.radius * 2 * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius = " + this.radius + ", which is a subclass of " + super.toString();
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
